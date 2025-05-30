8294abf811833433bf23ed037b59e88b5c676cba
oheil <git@heilbit.de>
2025-05-30 14:13:19 +0200

    added tempconv example

---
+++ examples/7guis/tempconv.jl
@@ -0,0 +1,10 @@
+using Slint
+
+slintFile = "examples\\7guis\\tempconv.slint"
+startComponent = "TempConv"
+
+Slint.compile_from_file(slintFile,startComponent)
+
+Slint.run()
+# unload library
+Slint.close()
