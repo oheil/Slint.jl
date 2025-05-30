f2d61f1afc9a4bd1c4146adf6a104412ac4f8f1d
oheil <git@heilbit.de>
2025-05-30 14:16:41 +0200

    added timer example

---
+++ examples/7guis/timer.jl
@@ -0,0 +1,10 @@
+using Slint
+
+slintFile = "examples\\7guis\\timer.slint"
+startComponent = "MainWindow"
+
+Slint.compile_from_file(slintFile,startComponent)
+
+Slint.run()
+# unload library
+Slint.close()
