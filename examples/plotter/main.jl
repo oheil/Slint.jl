7bc422fe2a18ae6c25c6f6a8d34f0798998bcbb8
Oliver Heil <git@heilbit.de>
2025-06-03 16:35:08 +0200

    Julia experiments to get an image buffer

--- examples/plotter/main.jl
+++ examples/plotter/main.jl
@@ -12,6 +12,27 @@ function on_render_plot(params...)
         println(p," ",typeof(p))
     end
 
+    # https://beautiful.makie.org/dev/examples/3d/surfaces/complex_function
+    #   using CairoMakie
+    #   CairoMakie.activate!()
+    #   x = -2:0.005:2
+    #   y = -2:0.005:2
+    #   f(z) = (z^2 + 1) / (z^2 - 1)
+    #   fvals = [f(u + 1im * v) for u in x, v in y]
+    #   fvalues = abs.(fvals)
+    #   fargs = angle.(fvals)
+    #   indxCut = fvalues .> 3
+    #   fvalues[indxCut] .= 3.01
+    #   
+    #   fig, ax, pltobj = surface(x, y, fvalues, color = fargs,
+    #       colormap = :roma, colorrange = (-π, π),
+    #       backlight = 1.0f0, highclip = :black,
+    #       figure = (; size = (1200, 800), fontsize = 22));
+    #   myfig=Figure()  ???
+    #   img = colorbuffer(fig.scene);   ???
+    #   image(myfig[1,1],img,axis = (title = "Default",))  ???
+
+
     return C_NULL
 
     
