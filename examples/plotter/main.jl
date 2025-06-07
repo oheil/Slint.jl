37abac7c0da2225a6a741175deb808e6d1156690
oheil <git@heilbit.de>
2025-06-07 15:49:51 +0200

    nothing works

--- examples/plotter/main.jl
+++ examples/plotter/main.jl
@@ -1,18 +1,50 @@
 using Slint
 
-#using WGLMakie
-#using WGLMakie.Colors
+using GLMakie
+using GLMakie.Colors
 
 slintFile = "examples\\plotter\\plotter.slint"
 startComponent = "MainWindow"
 
 Slint.compile_from_file(slintFile,startComponent)
 
-#buffer = zeros(RGB24, 600, 800)   # 4 x w=800 x h=600 Bytes, sizeof(buffer)
+buffer = zeros(RGB24, 600, 800)   # 4 x w=800 x h=600 Bytes, sizeof(buffer)
 #buffer = zeros(ARGB32, 800, 600)  # 4 x 800 x 600 Bytes
-buffer = zeros(UInt32, 800, 600)   # 4 x 800 x 600 Bytes
+#buffer = zeros(UInt32, 800, 600)   # 4 x 800 x 600 Bytes
 
-#GLMakie.activate!(; visible = false)
+GLMakie.activate!(; visible = false)
+#fig = GLMakie.Figure(resolution = (800, 600), backgroundcolor = :black)
+#scr = GLMakie.Screen(fig.scene; start_renderloop=false)
+#fig = GLMakie.Figure(size = (800, 600))
+#scr = GLMakie.Screen(fig.scene; start_renderloop=false)
+
+function initFig()
+    fig = GLMakie.Figure(size = (800, 600))
+    scr = GLMakie.Screen(fig.scene, Makie.ImageStorageFormat; start_renderloop=false)
+    ax = Axis3(fig[1, 1], aspect = :data)
+    f = let fig = fig, scr = scr, ax = ax
+        () -> (fig, scr, ax)
+    end
+    return f
+end
+#GetFig = initFig()
+
+#ax = Axis3(fig[1, 1], aspect = :data)
+
+#pltobj = surface!(ax, x, y, fvalues, color = fargs,
+#    colormap = :roma, colorrange = (-π, π),
+#    backlight = 1.0f0, highclip = :black);
+#resize_to_layout!(fig)
+
+#buffer .= colorbuffer(fig.scene; scr.config)
+
+#fig, ax, pltobj = surface(x, y, fvalues, color = fargs,
+#    colormap = :roma, colorrange = (-π, π),
+#    backlight = 1.0f0, highclip = :black,
+#    figure = (; size = sz, fontsize = 22));
+
+#resize_to_layout!(fig)
+#scr = GLMakie.Screen(fig.scene; start_renderloop=false)
 
 # implementation of callback:
 #       pure callback render_plot(/* pitch */ float, /* yaw */ float, /* amplitude */ float) -> image;
@@ -80,7 +112,6 @@ function on_render_plot(params...)
 
     =#
 
-    #=
 	sz = (800, 600)
 
 	x = -2:0.005:2
@@ -91,20 +122,47 @@ function on_render_plot(params...)
 	fargs = angle.(fvals)
 	indxCut = fvalues .> 3
 	fvalues[indxCut] .= 3.01
-	fig, ax, pltobj = surface(x, y, fvalues, color = fargs,
-		colormap = :roma, colorrange = (-π, π),
-		backlight = 1.0f0, highclip = :black,
-		figure = (; size = sz, fontsize = 22));
 
-	resize_to_layout!(fig)
-	
-    #scr = WGLMakie.Screen(fig.scene)
+    #delete!(ax,pltobj)
+
+    #pltobj = surface!(ax, x, y, fvalues, color = fargs,
+    #    colormap = :roma, colorrange = (-π, π),
+    #    backlight = 1.0f0, highclip = :black);
+	#resize_to_layout!(fig)
+    GetFig = initFig()
+    fig, scr, ax = GetFig()
+
+    #fig, ax, pltobj = surface(x, y, fvalues, color = fargs,
+    #    colormap = :roma, colorrange = (-π, π),
+    #    backlight = 1.0f0, highclip = :black,
+    #    figure = (; size = sz, fontsize = 22));
+    #ax = Axis3(fig[1, 1], aspect = :data)
+    pltobj = surface!(ax, x, y, fvalues, color = fargs,
+        colormap = :roma, colorrange = (-π, π),
+        backlight = 1.0f0, highclip = :black);
+
+    #scr = GLMakie.Screen(fig.scene; start_renderloop=false)
+
+    resize_to_layout!(fig)
+
+    GLMakie.ShaderAbstractions.switch_context!(scr.glscreen)
+    ctex = scr.framebuffer.buffers[:color]
+    #GLMakie.GLFW.PostEmptyEvent()
+    #GLMakie.GLFW.PollEvents()
+    #GLMakie.pollevents(scr, Makie.BackendTick)
+    GLMakie.render_frame(scr, resize_buffers=false)
+    framecache = Matrix{RGB{Makie.N0f8}}(undef, size(ctex))
+    GLMakie.fast_color_data!(framecache, ctex)
+    buffer .= PermutedDimsArray(view(framecache, :, size(framecache, 2):-1:1), (2, 1))
+
+
+    #scr = GLMakie.Screen(fig.scene; start_renderloop=false)
     #colorbuffer(fig.scene)
-    #buffer .= colorbuffer(fig.scene)
+    #buffer .= colorbuffer(fig.scene; scr.config)
 
     #using ImageView
     #imshow(buffer)
-    =#
+    GLMakie.closeall()
 
     return buffer
     
