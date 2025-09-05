5d559c9279b76e8e60804e878b6c2e97b4767339
Oliver Heil <git@heilbit.de>
2025-09-05 20:33:03 +0200

    system specific build linux (WSL)

---
+++ deps/deps_linux.jl
@@ -0,0 +1,15 @@
+import Libdl
+
+const slintwrapper = joinpath(@__DIR__, "libslintwrapper.so")
+
+function check_deps()
+    global slintwrapper
+    if !isfile(slintwrapper)
+        error("$slintwrapper does not exist, Please re-run ENV[\"JULIA_SLINT_REBUILD\"]=1;Pkg.build(\"Slint\"), and restart Julia.")
+    end
+    handle = Libdl.dlopen_e(slintwrapper)
+    if handle == C_NULL
+        error("$slintwrapper cannot be opened, Please re-run ENV[\"JULIA_SLINT_REBUILD\"]=1;Pkg.build(\"Slint\"), and restart Julia.")
+    end
+    return handle
+end
